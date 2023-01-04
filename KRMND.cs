f1a3221a11c351ea3e1a0b36b6f878f64f0bf119
aliamnieh <117279015+aliamnieh@users.noreply.github.com>
2023-01-04 11:43:22 -0800

    Add files via upload

---
+++ KRMND.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace payment_of_karmand
+{
+    class KRMND
+    {
+        public int Id;
+        private ulong _basepay;
+        public string Name;
+        public string Family;
+        private double _maliat;
+        private const double insurance = 0.07;
+        public ulong basepay
+        {
+            get
+            {
+                return _basepay;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _basepay = value;
+                    if (value >= 6500000) _maliat = 0.1;
+                    else _maliat = 0;
+                }
+                else
+                {
+                    _basepay = value;
+                    Console.WriteLine("the value of basepay is invalid");
+                }           }
+        }
+        public double finalpay()
+        {
+            return (basepay * (1 - (insurance + _maliat)));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace payment_of_karmand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //برنامه ای بنویسید که در یک کلاس حقوق ناخالص کارمند را دریافت و حقوق نهایی را محاسبه کند
+            KRMND arash = new KRMND();
+            Console.WriteLine("enter the id of worker : ");
+            arash.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter name : ");
+            arash.Name = Console.ReadLine();
+            Console.WriteLine("enter family : ");
+            arash.Family = Console.ReadLine();
+            Console.WriteLine("enter basepay : ");
+            arash.basepay = ulong.Parse(Console.ReadLine());
+            Console.WriteLine("the final pay is : " + arash.finalpay());
+            Console.ReadKey();
+        }
+    }
+}
